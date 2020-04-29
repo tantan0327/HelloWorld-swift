8752c153cf81faccf58e46f055f641d331607260
Tange <tantantatange@gmail.com>
2020-04-29 12:10:41 +0900

    tried text input & styling

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Bicycle for the Mind")
+                .font(.title)
+                .fontWeight(.heavy)
+            Text("知性の自転車")
+                .foregroundColor(Color.orange)
+            Text("The Quick Brown Fox Jumps Over the Lazy Dog")
+                .font(.largeTitle)
+            .multilineTextAlignment(.center)
+                .frame(width: 200.0)
+        }
     }
 }
 
