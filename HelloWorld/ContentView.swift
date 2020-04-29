8e9c0f9d449801d33e30b49d76ca97c86d076c09
Tange <tantantatange@gmail.com>
2020-04-29 10:08:21 +0900

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by 丹下敦矢 on 2020/04/29.
+//  Copyright © 2020 丹下敦矢. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
