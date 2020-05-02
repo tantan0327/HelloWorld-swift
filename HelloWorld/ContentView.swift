cb784ff97360d8d9f7bc3d69b3e873d8b78d310c
Tange <tantantatange@gmail.com>
2020-05-02 11:33:45 +0900

    print image

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -11,18 +11,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            VStack(alignment: .trailing) {
-                Text("Bicycle for the Mind")
-                Text("知性の自転車")
-                    .offset(x: -10.0, y: 0)
+            VStack {
+                VStack(alignment: .trailing) {
+                    Text("Bicycle for the Mind")
+                    Text("知性の自転車")
+                        .offset(x: -10.0, y: 0)
+                }
+                .padding(.top, 80)
+                VStack(alignment: .trailing) {
+                    Text("Pity is akin to love")
+                    Text("哀れみは恋の始まり")
+                        .offset(x: -10, y: 0)
+                }
+                .padding(.top, 20)
             }
-            .padding(.top, 80)
-            VStack(alignment: .trailing) {
-                Text("Pity is akin to love")
-                Text("哀れみは恋の始まり")
-                    .offset(x: -10, y: 0)
-            }
-            .padding(.top, 20)
+            Image("brain")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 400)
+                .clipped()
+                .overlay(
+                    Text("Hello World")
+                        .font(.largeTitle)
+                        .fontWeight(.light)
+                        .offset(x: 0, y: -50)
+                      .foregroundColor(Color.red)
+                )
         }
     }
 }
