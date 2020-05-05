0b94077d758c79967e46d0fc8d0e04dcf31f39dd
Tange <tantantatange@gmail.com>
2020-05-05 12:18:28 +0900

    implemented stepper

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,88 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var num:Int = 0
-    @State var msg = "Thank you!"
-    @State var iLike = true
-    @State var isFast = true
+    @State var kosu:Int = 0
+    let tanaka = 240
+    let tax = 0.1
     
     var body: some View {
         VStack {
-            Button(action: {
-                self.num = Int.random(in: 0...100)
-            }) {
-                Text("Random Button")
-                    .font(.largeTitle)
-                    .frame(width: 280, height: 60, alignment: .center)
-                    .foregroundColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(15, antialiased: true)
-            }
-            Text("\(num)")
-                .font(.largeTitle)
-                .padding()
-            Divider()
-
-            Button("Tap") {
-                if self.msg == "Thank you!" {
-                    self.msg = "ありがとう!"
-                } else {
-                    self.msg = "Thank you!"
-                }
-            }
-                .font(.headline)
-                .foregroundColor(.white)
-                .background(
-                    Capsule()
-                        .foregroundColor(.green)
-                        .frame(width: 80, height: 30)
-                )
-            Text(msg).padding()
-
-            Divider()
-
-            // $でオブジェクトそのものにアクセスし、現在の値をを取得
-            Toggle(isOn: $iLike) {
-                Text("Like or Not")
-                    .font(.largeTitle)
-            }
-            .fixedSize()
-            .padding(50)
-
-
-            if iLike {
-                Image(systemName: "heart.fill")
-                    .font(.system(size: 80))
-                    .foregroundColor(.red)
-            } else {
-                Image(systemName: "heart.slash")
-                    .font(.system(size: 80))
-                    .foregroundColor(.gray)
-            }
-
-            Divider()
+            Stepper(
+                onIncrement: {
+                    self.kosu += 5
+                    self.kosu = min(self.kosu, 30)
+                },
+                onDecrement: {
+                    self.kosu -= 5
+                    self.kosu = max(self.kosu, 0)
+                },
+                label: {Text("個数: \(self.kosu)")}
+            ).frame(width: 200)
             
-            HStack {
-                Button(action: {
-                    withAnimation {
-                        self.isFast.toggle()
-                    }
-                }) {
-                    Image(systemName: isFast ? "hare" : "tortoise")
-                        .foregroundColor(isFast ? .blue : .black)
-                        .rotationEffect(.degrees(isFast ? 0 : 180))
-                        .scaleEffect(3)
-                }
-                .padding(30)
-                
-                Text(isFast ? "Fast" : "Slow")
-                    .font(.title)
-                    .foregroundColor(isFast ? .pink : .gray)
-                    .rotationEffect(.degrees(isFast ? 0 : 180))
-            }
+            Text("料金: \(calc(self.kosu))円[税込み]")
+                .underline()
         }
     }
+    
+    func calc(_ num:Int) -> Int {
+        let price = self.tanaka * num
+        let result = Double(price) * (1 + self.tax)
+        return Int(result)
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
