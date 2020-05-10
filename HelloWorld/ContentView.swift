e119b7444d33231d981fae0623919988a6ff0d32
Tange <tantantatange@gmail.com>
2020-05-10 11:37:15 +0900

    added timePublisher

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
  
 struct ContentView: View {
     @ObservedObject var stopwatch = Stopwatch()
+    let currentTimePublisher = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .default).autoconnect()
+    
+    @State private var now = Date()
     
     var body: some View {
         VStack {
@@ -34,6 +37,17 @@ struct ContentView: View {
             }
             .frame(width: 200)
             Text("\(self.stopwatch.counter)")
+            
+            Divider()
+            
+            VStack {
+                Text("Time is").font(.largeTitle)
+                Text("\(now.description)")
+            }
+            .onReceive(currentTimePublisher) { date in
+                self.now = date
+            }
+            
         }.font(.largeTitle)
         
     }
